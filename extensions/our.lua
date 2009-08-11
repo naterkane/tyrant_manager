0412102ab8dc9a101d2835d32cc86a9dbfeed013
amix <amix@5aeed74a-6033-4279-9a5d-c50fcf008bc4>
2009-08-11 12:43:46 +0000

    Applied some optimization to list feature so they use putcat to add and not put (this should reduce load).

--- extensions/our.lua
+++ extensions/our.lua
@@ -1,6 +1,5 @@
 -- Misc. Extensions
 --
-
 function incr(key, value)
    value = tonumber(value)
    if not value then
@@ -22,22 +21,6 @@ end
 --
 LIST_DELIM = "~"
 
-function _current_list(key, tokens)
-   local old = _get(key)
-
-   if old then
-       for i = 1, #tokens do
-            old = string.gsub(old, "^" .. tokens[i] .. LIST_DELIM, "")
-            old = string.gsub(old, LIST_DELIM .. tokens[i] .. LIST_DELIM, LIST_DELIM)
-       end
-
-       return _tokenize(old, LIST_DELIM)
-    else
-        return false
-   end
-end
-
-
 function list_add(key, value)
     --- Determine the limit by inspecting if | is in the key
     local limit = 200
@@ -48,26 +31,41 @@ function list_add(key, value)
         key = key_parts[2]
     end
 
-    --- Fetch the current or create a new list
-    local tokens = _tokenize(value, LIST_DELIM)
-    local current = _current_list(key, tokens)
+    local size_key = key .. "size"
 
-    if current == false then
-        current = {}
-    end
+    local list_size = _get(size_key)
 
-    --- Insert the new elements and remove the old if the size is too big
-    local insert_index = 1
-    for i = 1, #tokens do
-        table.insert(current, insert_index, tokens[i])
-        insert_index = insert_index + 1
-        if #current > limit then
-            table.remove(current)
+    --- If the size is not initialized, then init it
+    if list_size == nil then
+        local current = _current_list(key, nil)
+
+        -- We need to reverse the current list, since
+        -- the first version of lists did not use putcat
+        -- to append values at the end...
+        -- It added them to the front.
+        if #current > 0 then
+            current = _list_reverse(key, current)
         end
+
+        list_size = #current
+        _put(size_key, list_size)
+    else
+        list_size = tonumber(list_size)
     end
 
-    local result = table.concat(current, LIST_DELIM)
-    _put(key, result .. LIST_DELIM)
+    --- If the size is too big then clean up in it
+    local values = _tokenize(value, LIST_DELIM)
+
+    if list_size > (limit+100) then
+        local new_size = _list_cleanup(key, limit, #values)
+        _put(size_key, new_size)
+    else
+        list_size = list_size + #values
+        _put(size_key, list_size)
+    end
+
+    --- Append values at the end of the list
+    _putcat(key, value)
     return "ok"
 end
 
@@ -103,3 +101,55 @@ function _tokenize(text, delims)
     end 
     return tokens 
 end 
+
+function _current_list(key, tokens)
+   local old = _get(key)
+
+   if old then
+       if tokens then
+           for i = 1, #tokens do
+                old = string.gsub(old, "^" .. tokens[i] .. LIST_DELIM, "")
+                old = string.gsub(old, LIST_DELIM .. tokens[i] .. LIST_DELIM, LIST_DELIM)
+           end
+       end
+
+       return _tokenize(old, LIST_DELIM)
+    else
+        return {}
+   end
+end
+
+--- Deletes the first entries in the list
+--- so there's only `limit` entires left in the list
+function _list_cleanup(key, limit, new_items_size)
+    local current = _current_list(key, nil)
+    local current_size = #current + new_items_size
+
+    while current_size > limit do
+        --remove the first item
+        table.remove(current, 1)
+        current_size = current_size - 1
+    end
+
+    _list_store(key, current)
+
+    return current_size
+end
+
+function _list_reverse(key, t)
+	local size = #t
+
+	local j = size
+	for i = 1, size / 2 do
+		t[i], t[j] = t[j], t[i]
+		j = j - 1
+	end
+
+    _list_store(key, t)
+    return t
+end
+
+function _list_store(key, list)
+    local result = table.concat(list, LIST_DELIM)
+    _put(key, result .. LIST_DELIM)
+end
